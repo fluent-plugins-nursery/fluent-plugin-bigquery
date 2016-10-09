9931734ae06ac9bf6e9198bceac982e96e50cd7e
joker1007 <kakyoin.hierophant@gmail.com>
2016-10-09 19:08:33 +0900

    `load` mode supports v0.14 plugin style

--- lib/fluent/plugin/out_bigquery.rb
+++ lib/fluent/plugin/out_bigquery.rb
@@ -42,7 +42,7 @@ module Fluent
       def configure_for_load(conf)
         raise ConfigError unless conf["method"] == "load"
 
-        buffer_config = conf.elements("buffer")
+        buffer_config = conf.elements("buffer")[0]
         return unless buffer_config
         buffer_config["@type"]                       = "file"        unless buffer_config["@type"]
         buffer_config["flush_mode"]                  = :interval     unless buffer_config["flush_mode"]
@@ -422,7 +422,7 @@ module Fluent
 
       module LoadImplementation
         def _write(chunk, table_id_format, _)
-          table_id = extract_placeholders(table_id_format, chunk)
+          table_id = extract_placeholders(table_id_format, chunk.metadata)
           load(chunk, table_id)
         end
 
@@ -453,7 +453,7 @@ module Fluent
         private
 
         def create_upload_source(chunk)
-          chunk_is_file = @buffer_type == 'file'
+          chunk_is_file = @buffer_config["@type"] == 'file'
           if chunk_is_file
             File.open(chunk.path) do |file|
               yield file
