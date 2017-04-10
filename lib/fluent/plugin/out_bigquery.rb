be4d8d42b2462eb78e67a08308b8ab3f7fed9816
Paul Bellamy <paul.a.bellamy@gmail.com>
2017-04-10 16:38:04 +0100

    rename insert_late_timestamp to add_insert_timestamp

--- lib/fluent/plugin/out_bigquery.rb
+++ lib/fluent/plugin/out_bigquery.rb
@@ -118,11 +118,11 @@ module Fluent
       # prevent_duplicate_load (only load)
       config_param :prevent_duplicate_load, :bool, default: false
 
-      # insert_late_timestamp (only insert)
-      # late-adds a timestamp just before sending, so that buffering time is
-      # not taken into account. Gives a field in bigquery which represents the
-      # insert time of the row.
-      config_param :insert_late_timestamp, :string, default: nil
+      # add_insert_timestamp (only insert)
+      # adds a timestamp just before sending the rows to bigquery, so that
+      # buffering time is not taken into account. Gives a field in bigquery
+      # which represents the insert time of the row.
+      config_param :add_insert_timestamp, :string, default: nil
 
       config_param :method, :enum, list: [:insert, :load], default: :insert, skip_accessor: true
 
@@ -404,11 +404,11 @@ module Fluent
 
       module InsertImplementation
         def _write(chunk, table_format)
-          now = Time.now.utc.strftime("%Y-%m-%d %H:%M:%S.%6N") if @insert_late_timestamp
+          now = Time.now.utc.strftime("%Y-%m-%d %H:%M:%S.%6N") if @add_insert_timestamp
           rows = chunk.open do |io|
             io.map do |line|
               record = MultiJson.load(line)
-              record[@insert_late_timestamp] = now if @insert_late_timestamp
+              record[@add_insert_timestamp] = now if @add_insert_timestamp
               row = {"json" => record}
               row["insert_id"] = @get_insert_id.call(record) if @get_insert_id
               row.deep_symbolize_keys
