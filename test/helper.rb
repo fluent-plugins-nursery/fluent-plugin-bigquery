c034375f50614c3b1b9994954949e349bb6d9a3e
Kenji Okimoto <okimoto@clear-code.com>
2017-05-19 10:16:33 +0900

    Use bunlder/setup instead of old code snippet

--- test/helper.rb
+++ test/helper.rb
@@ -1,11 +1,4 @@
-require 'bundler'
-begin
-  Bundler.setup(:default, :development)
-rescue Bundler::BundlerError => e
-  $stderr.puts e.message
-  $stderr.puts "Run `bundle install` to install missing gems"
-  exit e.status_code
-end
+require 'bundler/setup'
 require 'test/unit'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
