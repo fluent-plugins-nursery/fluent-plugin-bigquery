9040fa0618a3fb7b3465918b89e8e6b125359f2a
Kenji Okimoto <okimoto@clear-code.com>
2017-05-19 10:18:15 +0900

    Remove redundant code
    `Fluent::Test.setup` do same thing.

--- test/helper.rb
+++ test/helper.rb
@@ -4,15 +4,6 @@ require 'test/unit'
 $LOAD_PATH.unshift(File.join(__dir__, '..', 'lib'))
 $LOAD_PATH.unshift(__dir__)
 require 'fluent/test'
-unless ENV.has_key?('VERBOSE')
-  nulllogger = Object.new
-  nulllogger.instance_eval {|obj|
-    def method_missing(method, *args)
-      # pass
-    end
-  }
-  $log = nulllogger
-end
 
 require 'fluent/plugin/buffer'
 require 'fluent/plugin/buf_memory'
