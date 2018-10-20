185736bda51fc35f7bf29ab6b8a98a45572eead4
Adrián Matellanes <amatellanes@freepik.com>
2018-10-20 10:52:28 +0200

    Add time_partitioning_require_partition_filter support

--- test/plugin/test_out_bigquery_insert.rb
+++ test/plugin/test_out_bigquery_insert.rb
@@ -416,6 +416,7 @@ class BigQueryInsertOutputTest < Test::Unit::TestCase
       time_partitioning_type day
       time_partitioning_field time
       time_partitioning_expiration 1h
+      time_partitioning_require_partition_filter true
     CONFIG
 
     stub_writer do |writer|
